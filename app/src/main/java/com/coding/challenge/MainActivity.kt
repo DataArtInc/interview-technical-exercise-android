84ef9a9fed518de34e9112e5d5acb4ddc17c54d8
Eugene Vinogradny <eugene.vinogradny@thetrainline.com>
2025-08-19 21:23:07 +0200

    Updated MainActivity 4.

--- app/src/main/java/com/coding/challenge/MainActivity.kt
+++ app/src/main/java/com/coding/challenge/MainActivity.kt
@@ -37,7 +37,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MainScreen() {
-    // A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
