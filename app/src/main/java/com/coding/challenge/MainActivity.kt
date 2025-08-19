ce45425b26bf69b16b7734d57b0bd5ef2744e365
Eugene Vinogradny <eugene.vinogradny@thetrainline.com>
2025-08-19 21:23:42 +0200

    Updated MainActivity 5.

--- app/src/main/java/com/coding/challenge/MainActivity.kt
+++ app/src/main/java/com/coding/challenge/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
